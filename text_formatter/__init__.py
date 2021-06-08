8a17d9a05237ca823ef6b64fb34f1c65d527e080
Diego Ramirez <dr01191115@gmail.com>
2021-06-08 12:26:31 -0500

    Convert commentaries into docstrings
    It is just a trivial change... ignore it.

--- text_formatter/__init__.py
+++ text_formatter/__init__.py
@@ -1,5 +1,8 @@
-# text-formatter
-# A pretty formatter for Python strings.
+"""
+text-formatter
+-------------------------------------------
+A pretty formatter for Python strings.
+"""
 
 __author__ = "Diego Ramirez (dr01191115@gmail.com) @DiddiLeija"
 __version__ = "1.0.0"
