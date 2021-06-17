55f204627fa9e1a3c88325eada95e5abcd3e3782
Diego Ramirez <dr01191115@gmail.com>
2021-06-17 08:22:38 -0500

    Create `text_formatter.justify.justify()`
    View issue #1 to read the reasons for adding this.

--- text_formatter/justify.py
+++ text_formatter/justify.py
@@ -1,3 +1,9 @@
 """
 Justify the strings, according to a certain length.
 """
+
+def justify(s: str, l: int) -> str:
+    """
+    Justify a string (s) and make it
+    fit to a specific length (l).
+    """
