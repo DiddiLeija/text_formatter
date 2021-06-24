d99f32f452bf22f31d7bf3ece0390e2f16c149b6
Diego Ramirez <dr01191115@gmail.com>
2021-06-24 08:33:25 -0500

    Set up a proper version for text_formatter
    It will be "1.0.0dev4" by now, using "1.0.0" is just for the final release (which is not the case, until #1 and #7 are resolved).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="text_formatter",
-    version="1.0.0",
+    version="1.0.0dev4",
     author="Diego Ramirez",
     author_email="dr01191115@gmail.com",
     description="Pretty formatter for Python strings.",
