478d0b83e5abab91c6893587b5d1d355af3c210e
Diego Ramirez <dr01191115@gmail.com>
2021-07-19 09:06:30 -0500

    Add the `check.py` functions on `justify.py`
    The added function is "checkString()", which is being used on "justify()".

--- text_formatter/justify.py
+++ text_formatter/justify.py
@@ -4,9 +4,14 @@ Justify the strings, according to a certain length.
 
 __all__ = ["justify"]
 
+from text_formatter.check import checkString
+
 def justify(s: str, l: int) -> str:
     """
     Justify a string (s) and make it
     fit to a specific length (l).
     """
+    # check the string introduced
+    checkString(s)
+
     return s # This func needs larger development!!! (DiddiLeija/text_formatter#1)
