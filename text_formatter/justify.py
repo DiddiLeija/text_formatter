86754d12f0fa2eddd95449aa6d241df927c8a062
Diego Ramirez <dr01191115@gmail.com>
2021-08-10 12:12:24 -0500

    Update justify.py
    Make flake8 style fixes.

--- text_formatter/justify.py
+++ text_formatter/justify.py
@@ -6,12 +6,13 @@ __all__ = ["justify"]
 
 from text_formatter.check import checkString
 
-def justify(s: str, l: int) -> str:
+
+def justify(s: str, length: int) -> str:
     """
     Justify a string (s) and make it
-    fit to a specific length (l).
+    fit to a specific length (length).
     """
     # check the string introduced
     checkString(s)
 
-    return s # This func needs larger development!!! (DiddiLeija/text_formatter#1)
+    return s  # This func needs larger development!!! (DiddiLeija/text_formatter#1)
