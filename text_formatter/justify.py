512e8b1b3fe5d585874186d0ccabace360b9bb80
Diego Ramirez <dr01191115@gmail.com>
2021-08-26 14:00:30 -0500

    Update justify.py
    Sort the imports.

--- text_formatter/justify.py
+++ text_formatter/justify.py
@@ -4,7 +4,7 @@ Justify the strings, according to a certain length.
 
 __all__ = ["justify", "justify_bytes"]
 
-from text_formatter.check import checkString, checkBytes
+from text_formatter.check import checkBytes, checkString
 
 
 def justify(s: str, length: int) -> str:
