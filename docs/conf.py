e73cdecd9a7e85883ca123ba979a2c69d840fafe
Diego Ramirez <dr01191115@gmail.com>
2021-10-18 14:21:49 -0500

    Adopt Furo
    - Adopt Furo.
    - Add HTML options.

--- docs/conf.py
+++ docs/conf.py
@@ -17,3 +17,11 @@ language = "en"
 project = "text_formatter"
 author = "Diego Ramirez"
 copyright = "2021, Diego Ramirez"
+
+# HTML specs
+html_theme = "furo"
+#fmt: off
+html_theme_options = {
+    "announcement": "Do you like this? Tell us <a href='https://shoutouts.dev/projects/DiddiLeija/text_formatter'>here</a>!",
+}
+#fmt: on
