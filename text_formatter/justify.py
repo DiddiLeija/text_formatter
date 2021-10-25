93f02f9e3521c312c68a0e5fd2be605fd0a472e2
Diego Ramirez <dr01191115@gmail.com>
2021-10-25 12:37:04 -0500

    Complete the `justify.justify()` function
    Complete that function. Also, link the `justfy_bytes` to this function (so it becomes an "adapter"?).

--- text_formatter/justify.py
+++ text_formatter/justify.py
@@ -4,7 +4,17 @@ Justify the strings, according to a certain length.
 
 __all__ = ["justify", "justify_bytes"]
 
+from typing import List
+
 from text_formatter.check import checkBytes, checkString
+
+
+def line_to string(l: List[str]):
+    "Return a string from a list of strings."
+    final_string = ""
+    for char in l:
+        final_string += char
+    return final_string
 
 
 def justify(s: str, length: int) -> str:
@@ -14,8 +24,31 @@ def justify(s: str, length: int) -> str:
     """
     # check the string introduced
     checkString(s)
+    
+    for line in s.splitlines():
+        if len(line.lstrip()) < 1:
+            # there's nothing to do here
+            continue
 
-    return s  # This func needs larger development!!! (DiddiLeija/text_formatter#1)
+        while len(line) < length - 1:
+            # extend the spaces until
+            # they are enough...
+            char_count = 0
+            line_list = list(line)
+
+            for char in line_list:
+                if char == " ":
+                    line_list.insert(char_count, " ")
+                    char_count += 2
+                    continue
+                char_count += 1
+
+            line = list_to_string(line_list)
+
+            if len(line) > length - 1:
+                line.replace("  ", " ")
+
+    return s
 
 
 def justify_bytes(b: bytes, length: int) -> bytes:
@@ -26,6 +59,4 @@ def justify_bytes(b: bytes, length: int) -> bytes:
     """
     checkBytes(b)
 
-    # Just like justify(), this
-    # function is not completed yet.
-    return b
+    return justify(b.encode("utf-8"))
