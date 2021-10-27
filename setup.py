14d837d20d0c527a54c0378a2e090428cac09c3d
Diego Ramirez <dr01191115@gmail.com>
2021-10-27 13:04:00 -0500

    :label:
    Add more classifiers.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
